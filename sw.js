e74d84ac65f2f7568fbcd7d3e7b903f64f85f624
Yusinto Ngadiman <yusinto@gmail.com>
2018-09-19 15:05:48 +1000

    Updates

--- sw.js
+++ sw.js
@@ -25,44 +25,41 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4b4bb1145b0bccc211ed.js"
+    "url": "webpack-runtime-712a8f0941c8b7969b81.js"
   },
   {
     "url": "app.60176bdc619a5f4de8c3.css",
     "revision": "e46b9d811dd003878d588441d9b498a8"
   },
   {
-    "url": "app-04f7c7144301ca14671c.js"
+    "url": "app-ed278419a1d61a1fe0bd.js"
   },
   {
-    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-d289ef08d48f76646baf.js"
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-b968c701549028672092.js"
   },
   {
     "url": "index.html",
-    "revision": "057e8ef4430e5a8b87d3bac187f0c7f9"
+    "revision": "4aad9856801d52b315109784f830b483"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9e9236e95954c8aa70e108fae68a5b2c"
+    "revision": "a074f335805e722011c4163bb1e4d67f"
   },
   {
-    "url": "1-76d506966343745b8095.js"
+    "url": "0-284a09ba52ecb551f60a.js"
   },
   {
-    "url": "component---src-pages-index-js-b5923bb113f597e4850c.js"
+    "url": "component---src-pages-index-js-6b722811c5ff5d695745.js"
   },
   {
-    "url": "2-20587db370f10089c72b.js"
-  },
-  {
-    "url": "0-7c7fda51839ffae4a5f6.js"
+    "url": "1-b9d790e4adbe23d5dd29.js"
   },
   {
     "url": "static/d/965/path---index-6a9-RaLBWPTiKiRn50RXIkyPKmsoEx8.json",
     "revision": "29aaa84aa8dcbb6e9d09d3384ad85158"
   },
   {
-    "url": "component---src-pages-404-js-7301dc1ebd4e1ec81876.js"
+    "url": "component---src-pages-404-js-c121930d048d5ae1ae51.js"
   },
   {
     "url": "static/d/164/path---404-html-516-62a-NZuapzHg3X9TaN1iIixfv1W23E.json",
